b099bda3d5f84916594a14b3fe80ef037391eb3b
Faisal7560 <faisal.756009@gmail.com>
2026-05-27 10:30:48 +0300

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,1 +1,12 @@
-
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    const response = await env.ASSETS.fetch(request);
+    const newResponse = new Response(response.body, response);
+    newResponse.headers.set(
+      'Content-Security-Policy',
+      "default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src *;"
+    );
+    return newResponse;
+  }
+}
